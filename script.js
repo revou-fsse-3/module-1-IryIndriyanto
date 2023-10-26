bba77f090aa460f2fb885131ea6c98d2745b5105
Iry Indriyanto <iry.indriyanto20@gmail.com>
2023-10-26 10:42:35 -0700

    adding menu for mobile device (hamburger-menu)

---
+++ script.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("menu-mobile").style.width = "20rem";
+  }
+  
+  function closeNav() {
+    document.getElementById("menu-mobile").style.width = "0";
+  }
